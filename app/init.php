e8fc118a6ed197c5f87a6f695b1adc12b0fd052f
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-07 02:25:00 +0700

    membuat folder app dan public berserta folder dan file di dalam folder tersebut

---
+++ app/init.php
@@ -0,0 +1,4 @@
+<?php
+
+	require_once 'core/App.php'; // app -> kelas. Dua kelas utama pembentuk mvs
+	require_once 'core/Controller.php'; // Dua kelas utama pembentuk mvs
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php 
+
+	require_once '../app/init.php';
+
+	$app = new App; // menjalankan kelas App instansiasi kelas app
+
