c7f28ba4db54e9bb804a2f9f0b7f9d6a5dad7513
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-07 11:58:37 +0700

    menambahkan perintah di App.php dan menamabahkan file .htaccess di folder app dan public

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,16 @@
 
 	class App {
 		public function __construct() {
-			echo 'OK!';
+			$url = $this->parsURL();
+			var_dump($url);
+		}
+
+		public function parsURL() {
+			if( isset($_GET['url']) ) {
+				$url = rtrim($_GET['url'], '/');
+				$url = filter_var($url, FILTER_SANITIZE_URL);
+				$url = explode('/', $url);
+				return $url;
+			} 
 		}
 	}
