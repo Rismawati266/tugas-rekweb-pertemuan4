aca08ac230a408df53dc353c97abda60db9e647a
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 11:11:53 +0700

    menambahkan data di file Mahasiswa.php, memasukkan data diri mahasiswa, dan menambahkan link untuk memanggil data diri mahasiswa di file Mahasiswa.index.php

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,3 +1,31 @@
 <?php 
 
-class Mahasiswa_model
+class Mahasiswa_model {
+	private $mhs = [
+		[
+			"nama" => "Rismawati",
+			"nrp" => "173040012",
+			"email" => "risma.wati266@mail.unpas.ac.id",
+			"jurusan" => "Teknik Informatika"
+		],
+
+		[
+			"nama" => "Yantivia Rinwi Yumame",
+			"nrp" => "173040011",
+			"email" => "yantifia@mail.unpas.ac.id",
+			"jurusan" => "Teknik Informatika"
+		],
+
+		[
+			"nama" => "Andika Wahyu Setiawan",
+			"nrp" => "173040021",
+			"email" => "andikawahyusetiawan@mail.unpas.ac.id",
+			"jurusan" => "Teknik Informatika"
+		]
+	];
+
+	public function getAllMahasiswa() {
+		return $this->mhs;
+	}
+
+}
