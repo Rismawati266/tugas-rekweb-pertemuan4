3e48168828298076fdae4adab203adae064d48fb
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 18:48:26 +0700

    menambahkan

--- app/core/Database.php
+++ app/core/Database.php
@@ -12,10 +12,16 @@
 
 		public function __construct() { // berisi koneksi ke database
 			// data source name
-				$dsn = 'mysql:host=localhost;dbname=tugas-rekweb-pertemuan4';// identitas server kita -> data source name
+				$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;// identitas server kita -> data source name
+
+				// $option -> mengoptimasi koneksi ke database kita
+				$option = [
+					PDO::ATTR_PERSISTENT => true,// UNTUK MEMBUAT KONEKSI DATABASE KITA TERJAGA TERUS
+					PDO::ATTR_ERRORMODE => PDO::ERRMODE_EXCEPTION
+				];
 
 				try { 
-					$this->dbh = new PDO($dsn, 'root', '');
+					$this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
 				} catch(PDOException $e) { 
 					die($e->getMessage()); 
 				}
