e94e6316be358c6321424097d1962a62b00e472c
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 18:37:07 +0700

    menimpa kodingan

--- app/core/Database.php
+++ app/core/Database.php
@@ -11,6 +11,13 @@
 		private $stmt;
 
 		public function __construct() { // berisi koneksi ke database
+			// data source name
+				$dsn = 'mysql:host=localhost;dbname=tugas-rekweb-pertemuan4';// identitas server kita -> data source name
 
+				try { 
+					$this->dbh = new PDO($dsn, 'root', '');
+				} catch(PDOException $e) { 
+					die($e->getMessage()); 
+				}
 		}
 	}
