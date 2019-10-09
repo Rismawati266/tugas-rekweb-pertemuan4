4838af5dba88cb5110d76b221c138b8d7884643d
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 12:07:39 +0700

    memasukkan perintah untuk bisa koneksi ke database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,13 +1,22 @@
 <?php 
 
 class Mahasiswa_model {
-	private $mhs = [
-		private $dbh; // database heandler->buat menampung koneksi ke database
-		private $stmt // buat menyimpan query
+		private $dbh; // database handler->buat menampung koneksi ke database
+		private $stmt; // buat menyimpan query
 
-		public function __construct() {
-			
+		public function __construct() { // koneksi ke database
+			// koneksi dulu ke database
+			$dsn = 'mysql:host=localhost;dbname=tugas-rekweb-pertemuan4';// identitas server kita -> data source name
+
+			try { // kita mengecek. Apakah koneksinya berhasil atau tidak
+				$this->dbh = new PDO($dsn, 'root', '');
+			} catch(PDOException $e) { // jika error maka tangkap exceptionnya
+				die($e->getMessage()); // kita hentikan errornya.
+			}
 		}
+
+
+		//private $mhs = [
 
 	// 	[
 	// 		"nama" => "Yantivia Rinwi Yumame",
@@ -31,7 +40,9 @@ class Mahasiswa_model {
 	// ];
 
 	public function getAllMahasiswa() {
-		return $this->mhs;
+		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa'); // untuk mendapatkan semua data mahasiswa
+		$this->stmt->execute();
+		return $this->stmt->fetchAll(PDO::FETCH_ASSOC); // kita ambil semua datanya
 	}
 
 }
