b8b2f7fc1207d7e7b0c3b08a7f7383dafe707038
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 20:51:13 +0700

    mengubah mahasiswa/index menjadi mahasiswa/detail di file Mahasiswa.php

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -13,7 +13,7 @@
 			$data['judul'] = "Detail Mahasiswa";
 			$data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaById($id);
 			$this->view('templates/header', $data);
-			$this->view('mahasiswa/index', $data);
+			$this->view('mahasiswa/detail', $data);
 			$this->view('templates/footer');
 		}
 	}
