45da36bbc6483329bcb4008dcd088fcd3a7e793f
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 20:48:51 +0700

    menambahkan function getMahasiswaById

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -28,4 +28,10 @@ class Mahasiswa_model {
 		return $this->db->resultSet(); // tampilkan semua datanya
 	}
 
+	public function getMahasiswaById($id) {
+		$this->db->query(' SELECT * FROM ' . $this->table . ' WHERE id=:id');
+		$this->db->bind('id', $id);
+		return $this->db->single();
+	}
+
 }
