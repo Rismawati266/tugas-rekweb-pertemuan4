8a6c04f14096abc30574200dbf218b312f770bf6
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 18:21:33 +0700

    menambahkan define untuk menyimpan data-data dari database.

--- app/config/config.php
+++ app/config/config.php
@@ -3,3 +3,9 @@
 	// berisi data-data dari database berupa konstanta
 	define('BASEURL', 'http://localhost:8080/tugas-rekweb-pertemuan4/public');
  	
+ 	//DB DATABASE
+ 	define('DB_HOST', 'localhost');
+ 	define('DB_USER', 'root');
+ 	define('DB_PASS', '');
+ 	define('DB_NAME', 'tugas-rekweb-pertemuan4');
+ 	
