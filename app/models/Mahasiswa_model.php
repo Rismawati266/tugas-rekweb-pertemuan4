7e5cb6996767aabd4647248fac9e7b806770eda9
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 11:33:30 +0700

    menambahkan dbh, stmt, dan method function __construct()

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,27 +2,33 @@
 
 class Mahasiswa_model {
 	private $mhs = [
-		[
-			"nama" => "Rismawati",
-			"nrp" => "173040012",
-			"email" => "risma.wati266@mail.unpas.ac.id",
-			"jurusan" => "Teknik Informatika"
-		],
+		private $dbh; // database heandler->buat menampung koneksi ke database
+		private $stmt // buat menyimpan query
 
-		[
-			"nama" => "Yantivia Rinwi Yumame",
-			"nrp" => "173040011",
-			"email" => "yantifia@mail.unpas.ac.id",
-			"jurusan" => "Teknik Informatika"
-		],
+		public function __construct() {
+			
+		}
 
-		[
-			"nama" => "Andika Wahyu Setiawan",
-			"nrp" => "173040021",
-			"email" => "andikawahyusetiawan@mail.unpas.ac.id",
-			"jurusan" => "Teknik Informatika"
-		]
-	];
+	// 	[
+	// 		"nama" => "Yantivia Rinwi Yumame",
+	// 		"nrp" => "173040011",
+	// 		"email" => "yantifia@mail.unpas.ac.id",
+	// 		"jurusan" => "Teknik Informatika"
+	// 	],
+	// 	[
+	// 		"nama" => "Rismawati",
+	// 		"nrp" => "173040012",
+	// 		"email" => "risma.wati266@mail.unpas.ac.id",
+	// 		"jurusan" => "Teknik Informatika"
+	// 	],
+
+	// 	[
+	// 		"nama" => "Andika Wahyu Setiawan",
+	// 		"nrp" => "173040021",
+	// 		"email" => "andikawahyusetiawan@mail.unpas.ac.id",
+	// 		"jurusan" => "Teknik Informatika"
+	// 	]
+	// ];
 
 	public function getAllMahasiswa() {
 		return $this->mhs;
