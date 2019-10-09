74049e6d722f48592628827ddf90085b482691a2
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 18:33:36 +0700

    mengcut variabel dan function construct di file Mahasiswa_model.php

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -4,40 +4,17 @@ class Mahasiswa_model {
 		// private $dbh; // database handler->buat menampung koneksi ke database
 		// private $stmt; // buat menyimpan query
 
-		public function __construct() { // koneksi ke database
-			// koneksi dulu ke database
-			$dsn = 'mysql:host=localhost;dbname=tugas-rekweb-pertemuan4';// identitas server kita -> data source name
+		// public function __construct() { // koneksi ke database
+		// 	// koneksi dulu ke database
+		// 	$dsn = 'mysql:host=localhost;dbname=tugas-rekweb-pertemuan4';// identitas server kita -> data source name
 
-			try { // kita mengecek. Apakah koneksinya berhasil atau tidak
-				$this->dbh = new PDO($dsn, 'root', '');
-			} catch(PDOException $e) { // jika error maka tangkap exceptionnya
-				die($e->getMessage()); // kita hentikan errornya.
-			}
-		}
+		// 	try { // kita mengecek. Apakah koneksinya berhasil atau tidak
+		// 		$this->dbh = new PDO($dsn, 'root', '');
+		// 	} catch(PDOException $e) { // jika error maka tangkap exceptionnya
+		// 		die($e->getMessage()); // kita hentikan errornya.
+		// 	}
+		// }
 
-
-		//private $mhs = [
-
-	// 	[
-	// 		"nama" => "Yantivia Rinwi Yumame",
-	// 		"nrp" => "173040011",
-	// 		"email" => "yantifia@mail.unpas.ac.id",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	],
-	// 	[
-	// 		"nama" => "Rismawati",
-	// 		"nrp" => "173040012",
-	// 		"email" => "risma.wati266@mail.unpas.ac.id",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	],
-
-	// 	[
-	// 		"nama" => "Andika Wahyu Setiawan",
-	// 		"nrp" => "173040021",
-	// 		"email" => "andikawahyusetiawan@mail.unpas.ac.id",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	]
-	// ];
 
 	public function getAllMahasiswa() {
 		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa'); // untuk mendapatkan semua data mahasiswa
