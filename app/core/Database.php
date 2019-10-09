62595e1109a33452ef6f1fc51ee17ffd36393e6c
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 18:55:18 +0700

    menambahkan function query

--- app/core/Database.php
+++ app/core/Database.php
@@ -12,18 +12,22 @@
 
 		public function __construct() { // berisi koneksi ke database
 			// data source name
-				$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;// identitas server kita -> data source name
+			$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;// identitas server kita -> data source name
 
-				// $option -> mengoptimasi koneksi ke database kita
-				$option = [
-					PDO::ATTR_PERSISTENT => true,// UNTUK MEMBUAT KONEKSI DATABASE KITA TERJAGA TERUS
-					PDO::ATTR_ERRORMODE => PDO::ERRMODE_EXCEPTION
-				];
+			// $option -> mengoptimasi koneksi ke database kita
+			$option = [
+				PDO::ATTR_PERSISTENT => true,// UNTUK MEMBUAT KONEKSI DATABASE KITA TERJAGA TERUS
+				PDO::ATTR_ERRORMODE => PDO::ERRMODE_EXCEPTION
+			];
 
-				try { 
-					$this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
-				} catch(PDOException $e) { 
-					die($e->getMessage()); 
-				}
+			try { 
+				$this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
+			} catch(PDOException $e) { 
+				die($e->getMessage()); 
+			}
+		}
+
+		public function query($query) {// buat untuk jenerik. Querynya bisa di pake apapun, baik itu select, updatde, delete, insert. Inilah tujuan membuat database Wrapper supaya bisa di pakai secara fleksibel
+			$this->stmt = $this->dbh->prepare($query);
 		}
 	}
