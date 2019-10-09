52d219e26e71f515a8885dcadda854346907962d
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-09 19:27:55 +0700

    menambahkan require_once di file init.php

--- app/init.php
+++ app/init.php
@@ -2,4 +2,5 @@
 
 	require_once 'core/App.php'; // app -> kelas. Dua kelas utama pembentuk mvs
 	require_once 'core/Controller.php'; // Dua kelas utama pembentuk mvs
+	require_once 'core/Database.php';
 	require_once 'config/config.php';
