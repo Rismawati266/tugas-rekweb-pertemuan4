d308adde7199b4f9297a4bec5a12974f746d51b2
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-10 10:17:25 +0700

    mengubah tampilan ketika di klik berubah menjadi tambah data mahasiswa dan ubah data mahasiswa

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -8,7 +8,7 @@
 
 	<div class="row">
 		<div class="col-lg-6">
-			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+			<button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
 				Tambah Data Mahasiswa
 			</button>
 			<br></br>
@@ -29,11 +29,12 @@
 
 </div>
 
+<!-- Modal -->
 <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,11 @@
 $(function() {
 
+	$('.tampilTtambahData').on('click', function() {
+		$('#formModalLabel').html('Tambah Data Mahasiswa');
+	});
+
 	$('.tampilModalUbah').on('click', function() {
+		$('#formModalLabel').html('Ubah Data Mahasiswa');
 		console.log('ok');
 	})
 
