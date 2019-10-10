e3717a8f96a6b4aaddc9b61dad2ccc374c6220e2
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-10 22:36:47 +0700

    tugas mvc pertemuan ke 4 memperbaiki kodingan yang salah dan mencari kodingan yang salah

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ $(function() {
 	$('.tampilTtambahData').on('click', function() {
 		$('#formModalLabel').html('Tambah Data Mahasiswa');
 		$('.modal-footer buton[type=submit]').html('Tambah Data');
+		$('.modal-body form').attr('action', 'http://localhost:8080/tugas-rekweb-pertemuan4/public/mahasiswa/ubah')
 	});
 
 
@@ -23,6 +24,7 @@ $(function() {
 				$('#nrp').val(data.nrp);
 				$('#email').val(data.email);
 				$('#jurusan').val(data.jurusan);
+				$('#id').val(data.id);
 
 			}
 		});
