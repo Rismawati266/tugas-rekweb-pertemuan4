cc8bc8b016e6ce2b3d20056e55116c67adf55bcd
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-10 10:25:05 +0700

    menampilkan tambah data dan ubah data ketika menu tersebut di klik

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,13 @@ $(function() {
 
 	$('.tampilTtambahData').on('click', function() {
 		$('#formModalLabel').html('Tambah Data Mahasiswa');
+		$('.modal-footer buton[type=submit]').html('Tambah Data');
 	});
 
 	$('.tampilModalUbah').on('click', function() {
 		$('#formModalLabel').html('Ubah Data Mahasiswa');
-		console.log('ok');
-	})
+		$('.modal-footer button[type=submit]').html('Ubah Data');
+		//console.log('ok');
+	});
 
 });
